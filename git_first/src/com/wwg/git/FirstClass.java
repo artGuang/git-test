f3e6572cfe9afbbc77e6c98b27df999d76be7aae
王文广 <442281382@qq.com>
2019-07-15 18:28:20 +0800

    把本地工程添加到git 第一次修改

---
+++ git_first/src/com/wwg/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.wwg.git;
+
+public class FirstClass
+{
+    public static void main(String[] args) {
+        System.out.println("i am the best");
+        System.out.println("i am the best");
+        System.out.println("i am the best");
+        System.out.println("i am the best");
+
+    }
+    public void test(){
+        int i=0;
+        i+=10;
+    }
+}
